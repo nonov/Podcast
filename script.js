93cdfcedc97725b40bb7f8054fd207cc6e380505
Aïssa HENNI <aissa.henni@hotmail.com>
2015-03-15 11:30:49 +0100

    Affichage des informations channel

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 window.addEventListener("load", function() {
 	var btn = document.getElementsByName("submit")[0];
 	btn.addEventListener("click", function() {
@@ -23,8 +25,28 @@ window.addEventListener("load", function() {
 });
 
 function afficher(podcast) {
-	var div = document.getElementById("content");
-	div.textContent = podcast.getElementsByTagName("title")[0].textContent;
+	var contenu = document.getElementById("Channelcontent");
+	var tabChannel = podcast.getElementsByTagName("channel"); //Tableau des channel même si il n'y en à qu'une en général
+	for (var chaine in tabChannel){
+   		console.log("chaine");
+   		var titreNode = contenu.children[0];
+   		var linkNode = contenu.children[1];
+   		var descriptionNode = contenu.children[2];
+   		var imageNode = contenu.children[3];
+   		var itemNode = contenu.children[4];
+   		//var itemNodeVide = itemNode.cloneNode(true);
+
+   		titreNode.textContent = podcast.getElementsByTagName("title")[0].textContent;
+   		linkNode.textContent = podcast.getElementsByTagName("link")[0].textContent;
+   		descriptionNode.textContent = podcast.getElementsByTagName("description")[0].textContent;
+		
+		//Test si il existe une image
+		if (podcast.getElementsByTagName("image")[0] !== null){
+			console.log("Il y a une image");
+			var image = podcast.getElementsByTagName("image")[0];
+			imageNode.src = image.getElementsByTagName("url")[0].textContent;
+		}
+	}
 }
 
 function handleError(req) {
