3af2a627ae62fd5d82b599de77d7f28476941b14
Aïssa HENNI <aissa.henni@hotmail.com>
2015-03-15 12:26:53 +0100

    Boucle pour chaque item à completer

--- script.js
+++ script.js
@@ -27,14 +27,15 @@ window.addEventListener("load", function() {
 function afficher(podcast) {
 	var contenu = document.getElementById("Channelcontent");
 	var tabChannel = podcast.getElementsByTagName("channel"); //Tableau des channel même si il n'y en à qu'une en général
-	for (var chaine in tabChannel){
+	console.log("Nombre de channel: " + tabChannel.length);
+	for (var i, chaine; chaine=tabChannel[i];i++) {   //Pour chaque chaine
    		console.log("chaine");
    		var titreNode = contenu.children[0];
    		var linkNode = contenu.children[1];
    		var descriptionNode = contenu.children[2];
    		var imageNode = contenu.children[3];
-   		var itemNode = contenu.children[4];
-   		//var itemNodeVide = itemNode.cloneNode(true);
+   		var itemNode = document.getElementById("item");
+   		var itemNodeVide = itemNode.cloneNode(true);
 
    		titreNode.textContent = podcast.getElementsByTagName("title")[0].textContent;
    		linkNode.textContent = podcast.getElementsByTagName("link")[0].textContent;
@@ -45,6 +46,13 @@ function afficher(podcast) {
 			console.log("Il y a une image");
 			var image = podcast.getElementsByTagName("image")[0];
 			imageNode.src = image.getElementsByTagName("url")[0].textContent;
+		}
+
+		for (var item in podcast.getElementsByTagName("item")){  //Pour chaque item
+			console.log("Nombre d'item: " + podcast.getElementsByTagName("item").length);
+			//Nouvel item
+			var newItem = itemNodeVide.cloneNode(true);
+			//Recuperer title, link, description et enclosure et les insérer dans newItem
 		}
 	}
 }
